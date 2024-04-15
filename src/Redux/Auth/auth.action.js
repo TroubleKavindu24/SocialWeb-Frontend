571c936f5641e11d3e6f19adac8dafc34cbcd334
TroubleKavindu24 <it21834660@my.sliit.lk>
2024-04-15 15:03:06 +0530

    Get user details throw JWT

--- src/Redux/Auth/auth.action.js
+++ src/Redux/Auth/auth.action.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { API_BASE_URL } from "../../config/api"
-import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS } from "./auth.actionType"
+import { GET_PROFILE_FAILURE, GET_PROFILE_REQUEST, GET_PROFILE_SUCCESS, LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS } from "./auth.actionType"
 
 export const loginUserAction=(loginData)=>async(dispatch)=>{
     dispatch({type:LOGIN_REQUEST});
@@ -16,9 +16,9 @@ export const loginUserAction=(loginData)=>async(dispatch)=>{
 
     } catch (error) {
         console.log("-----------", error);
-        dispatch({type:LOGIN_FAILURE, payload:error})
+        dispatch({type:LOGIN_FAILURE, payload:error});
     }
-}
+};
 
 
 
@@ -39,4 +39,26 @@ export const registerUserAction=(loginData)=>async(dispatch)=>{
         console.log("-----------", error);
         dispatch({type:LOGIN_FAILURE, payload:error});
     }
-}
+};
+
+
+export const getProfileAction=(jwt)=>async(dispatch)=>{
+    dispatch({type:GET_PROFILE_REQUEST});
+    try {
+        const {data}=await axios.get(`${API_BASE_URL}/auth/users/profile`,
+        {
+            headers:{
+                Authorization:`Bearer ${jwt}`,
+            },
+        });
+
+        
+        console.log("profile----", data);
+
+        dispatch({type:GET_PROFILE_SUCCESS, payload:data});
+
+    } catch (error) {
+        console.log("-----------", error);
+        dispatch({type:GET_PROFILE_FAILURE, payload:error});
+    }
+};
